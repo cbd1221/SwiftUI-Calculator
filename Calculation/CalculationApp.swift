53573e7ba2cdc5a6de7f8268d0da0eae4adf01d9
Colin Dively <colinblairdively@gmail.com>
2022-09-19 23:33:08 -0400

    Initial Commit

---
+++ Calculation/CalculationApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculationApp.swift
+//  Calculation
+//
+//  Created by Colin Dively on 9/19/22.
+//
+
+import SwiftUI
+
+@main
+struct CalculationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
